0b48193f765de91ed6d57aedaefef44e6441b9e6
Ilya Belikov <ilya.belikov@phystech.edu>
2018-02-13 10:43:12 +0300

    statstools module & example

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 
 setup(
     name='cryptomipt',
-    version='0.1.2',
+    version='0.2.0',
     packages=['cryptomipt'],
     long_description=open(join(dirname(__file__), 'README.md')).read(),
 )
